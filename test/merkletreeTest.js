f16666b7f94331a6a5b7296bb85345e11005bf1c
AmitYYZ <amitshah0t7@gmail.com>
2018-03-15 17:41:46 -0400

    initial commit of merkle tree test

---
+++ test/merkletreeTest.js
@@ -0,0 +1,42 @@
+var test = require('tape');
+var merkletree_lib = require('../src/merkletree');
+
+
+var elements = [];
+for(var i=0; i < 10; i++){
+  elements.push(util.sha3("elem"+i));
+}
+var merkletree = new merkletree_lib.MerkleTree(elements);
+
+merkletree.generateHashTree();
+
+printTree(merkletree);
+
+var proof = merkletree.generateProof(elements[9]);
+console.log("=============== MERKLE PROOF GENERATED =================");
+for(var i =0; i < proof.length; i++){
+  var p = proof[i];
+  console.log(util.addHexPrefix(p.toString('hex')))
+}
+debugger;
+var verified= merkletree_lib.checkMerkleProof(proof,merkletree.getRoot(),elements[9], 5+1 );
+debugger;
+
+proof = merkletree.generateProof(merkletree.levels[0][9]);
+verified = merkletree_lib.checkMerkleProof(proof,merkletree.getRoot(),merkletree.levels[0][9], 9+1);
+debugger;
+
+merkletree = new merkletree_lib.MerkleTree(elements,true);
+
+merkletree.generateHashTree();
+printTree(merkletree);
+
+var proof = merkletree.generateProof(elements[9]);
+console.log("=============== MERKLE PROOF GENERATED =================");
+for(var i =0; i < proof.length; i++){
+  var p = proof[i];
+  console.log(util.addHexPrefix(p.toString('hex')))
+}
+debugger;
+var verified= merkletree_lib.checkMerkleProof(proof,merkletree.getRoot(),elements[9], 9+1 );
+debugger;
