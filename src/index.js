e6f786968050d7057cafda6a6f4af5511e7e7be4
Amit Shah <amitshah0t7@gmail.com>
2018-04-16 18:13:21 -0500

    update index.js to include channel package in entirety so we can reference the constants

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const Engine = require('./engine').Engine;
 const message = require ('./message');
-const Channel = require('./channel').Channel;
+const channel = require('./channel');
+const Channel = channel.Channel;
 const stateMachine = require('./stateMachine/stateMachine');
 const ChannelState = require('./channelState').ChannelState;
 
 module.exports={
-  Engine,Channel,ChannelState,message,stateMachine
+  Engine,Channel,ChannelState,message,stateMachine,channel
 }
