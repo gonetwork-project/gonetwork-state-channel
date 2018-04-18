3946a456484d0af85ce31fa2c17324da4cbcad99
Amit Shah <amitshah0t7@gmail.com>
2018-04-18 00:35:27 -0500

    add merkletree module to import list

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const channel = require('./channel');
 const Channel = channel.Channel;
 const stateMachine = require('./stateMachine/stateMachine');
 const ChannelState = require('./channelState').ChannelState;
-
+const merkletree = require('./merkletree');
 module.exports={
-  Engine,Channel,ChannelState,message,stateMachine,channel
+  Engine,Channel,ChannelState,message,stateMachine,channel,merkletree
 }
